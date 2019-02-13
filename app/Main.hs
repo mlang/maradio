0463cdd4bdd189888c31c8ab3875914f700b4450
Mario Lang <mlang@blind.guru>
2019-02-13 09:17:47 +0100

    all

--- app/Main.hs
+++ app/Main.hs
@@ -30,11 +30,14 @@ args Config {..} =
   Options <$> optional (strOption (  long "duration"
                                   <> metavar "[[hh:]mm:]ss"
                                   <> help "Only play for specified duration"))
-          <*> (subparser . mconcat . map \s@Station {..} -> command name $ info (pure s) mempty) stations
+          <*> (station stations <|> pure stations)
+ where
+  station stations = subparser . mconcat $
+    map (\s@Station{..} -> command name $ info (pure [s]) mempty) stations
 
 data Options = Options {
   duration :: Maybe String
-, station :: Station
+, station :: [Station]
 } deriving (Eq, Show)
 
 main :: IO ()
@@ -47,17 +50,14 @@ opts cfg = info (args cfg <**> helper) $ mempty
 play :: Config -> Options -> IO ()
 play Config{..} Options{..} = void $
   rawSystem "mpv" $
-  mpvOptions <> maybe [] (\d -> ["--length="<>d]) duration <> [url station]
-
-defaultConfig :: IO Config
-defaultConfig = do
-  let mpvOptions = ["-no-video", "-quiet"]
-  stations <- input auto "https://mlang.github.io/maradio/stations"
-  pure $ Config { .. }
+  mpvOptions <> maybe [] mpvLength duration <> map url station
+ where
+  mpvLength d = ["--length=" <> d]
 
 getConfig :: IO Config
 getConfig = do
   configFile <- getUserConfigFile "maradio" "config"
-  ifM (doesFileExist configFile)
-    (inputFile auto configFile)
-    defaultConfig
+  ifM (doesFileExist configFile) (inputFile auto configFile) do
+    let mpvOptions = ["-no-video", "-quiet"]
+    stations <- input auto "https://mlang.github.io/maradio/stations"
+    pure $ Config{..}
