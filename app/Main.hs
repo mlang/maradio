18d679b876dae44480fc3edd931d5dcdd52ab484
Mario Lang <mlang@blind.guru>
2019-02-20 23:56:34 +0100

    repl

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,7 @@ import Control.Monad.Extra (ifM)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Reader (ReaderT, runReaderT, ask)
 import Control.Monad.Reader.Class (MonadReader)
+import Control.Monad.State (StateT, evalStateT, gets, modify)
 import Control.Monad.STM (atomically)
 import Control.Monad.Trans.Class (lift)
 import Data.GI.Base.ManagedPtr (castTo)
@@ -30,10 +31,11 @@ import System.Directory (doesFileExist)
 import System.Environment.XDG.BaseDir
 import System.Process (readProcess)
 import Text.CLD2
-type URL = Prelude.String
+
+type URL = String
 
 data Config = Config {
-  mpvOptions :: [Prelude.String]
+  mpvOptions :: [String]
 , stations :: [Station]
 } deriving (Eq, Generic, Show)
 instance Interpret Config
@@ -45,7 +47,7 @@ data Station = Station {
 instance Interpret Station
 
 args :: Config -> Parser Options
-args Config {..} =
+args Config{..} =
   Options <$> optional (strOption (  long "duration"
                                   <> metavar "[[hh:]mm:]ss"
                                   <> help "Only play for specified duration"))
@@ -100,13 +102,28 @@ hline Options{..} = do
         externalPrint $ "Playing..."
       #setState playBin Gst.StatePlaying
 
-      hlineLoop pointedStation `runReaderT` playBin
+      repl `runReaderT` playBin `evalStateT` pointedStation
 
   _ <- GLib.sourceRemove watchId
   Gst.objectUnref bus
   #setState playBin Gst.StateNull
   Gst.objectUnref playBin
   #quit glibLoop
+
+data REPLCommand = Radio Radio
+data Radio = Play | Pause | Toggle | Next | Prev | URL Text
+
+radio :: Parser Radio
+radio = flag' Play (long "play")
+    <|> flag' Pause (long "pause")
+    <|> flag' Next (long "next")
+    <|> flag' Prev (long "prev")
+    <|> URL . pack <$> strArgument (metavar "URL")
+    <|> pure Toggle
+
+replCmd :: Parser REPLCommand
+replCmd = hsubparser $
+  command "radio" (info (Radio <$> radio) mempty)
 
 setURI :: (MonadReader Gst.Element m, MonadIO m) => Text -> m ()
 setURI uri = do
@@ -115,7 +132,7 @@ setURI uri = do
   liftIO $ setObjectPropertyString playBin "uri" (Just uri)
   void $ #setState playBin Gst.StatePlaying
 
-pause, play :: (MonadReader Gst.Element m, MonadIO m) => m ()
+pause, play, toggle :: (MonadReader Gst.Element m, MonadIO m) => m ()
 pause = do
   playBin <- ask
   void $ #setState playBin Gst.StatePaused
@@ -124,27 +141,47 @@ play = do
   playBin <- ask
   void $ #setState playBin Gst.StatePlaying
 
-hlineLoop :: PointedList Station -> ReaderT Gst.Element (InputT IO) ()
-hlineLoop pointedStation = do
-  minput <- lift $ getInputLine "% "
+toggle = do
+  playBin <- ask
+  (stateReturn, state, _) <- #getState playBin Gst.CLOCK_TIME_NONE
+  case stateReturn of
+    Gst.StateChangeReturnSuccess -> case state of
+      Gst.StateReady -> play
+      Gst.StatePaused -> play
+      Gst.StatePlaying -> pause
+      _ -> pure ()
+    _ -> pure ()
+    
+type REPL = ReaderT Gst.Element (StateT (PointedList Station) (InputT IO))
+
+repl :: REPL ()
+repl = do
+  minput <- lift . lift $ getInputLine "% "
   case minput of
     Nothing -> pure ()
-    Just "next" -> do
-      let pointedStation' = next pointedStation
-      lift . outputStrLn $ "Now playing " <> name (pointedStation' ^. focus)
-      setURI (pack . url $ pointedStation' ^. focus)
-      hlineLoop pointedStation'
-    Just "prev" -> do
-      let pointedStation' = previous pointedStation
-      lift . outputStrLn $ "Now playing " <> name (pointedStation' ^. focus)
-      setURI (pack . url $ pointedStation' ^. focus)
-      hlineLoop pointedStation'
-    Just "pause" -> pause >> hlineLoop pointedStation
-    Just "play" -> play >> hlineLoop pointedStation
-    Just "quit" -> pure ()
-    Just input -> do
-      lift . outputStrLn $ "Input was: " <> input
-      hlineLoop pointedStation
+    Just input -> case execParserPure Options.Applicative.defaultPrefs (info (replCmd <**> helper) mempty) (words input) of
+      Success cmd -> do
+        case cmd of
+          Radio rcmd -> case rcmd of
+            Play -> play
+            Pause -> pause
+            Toggle -> toggle
+            Next -> do
+              modify next
+              s <- gets (^. focus)
+              lift . lift . outputStrLn $ "Now playing " <> name s
+              setURI (pack . url $ s)
+            Prev -> do
+              modify previous
+              s <- gets (^. focus)
+              lift . lift . outputStrLn $ "Now playing " <> name s
+              setURI (pack . url $ s)
+            URL url -> setURI url
+        repl
+      Failure failure -> do
+        lift . lift . outputStrLn . fst $ renderFailure failure ""
+        repl
+      _ -> repl
 
 busCall :: (String -> IO ()) -> Gst.Element -> Gst.Bus -> Gst.Message -> IO Bool
 busCall externalPrint playBin bus message = do
